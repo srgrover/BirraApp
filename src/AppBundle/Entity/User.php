3e8198402af97570a7e29e99548ecaa12c827e6d
Miguel Angel Ortiz <ortiz.garcia.miguel.angel@gmail.com>
2018-03-08 20:08:17 +0100

    Añadidos getters y setters a la entidad User

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace AppBundle\Entity;
+
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="Usuario")
  */
-class User {
+class User
+{
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -122,4 +125,203 @@ class User {
      */
     protected $activated = false;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param bool $admin
+     * @return User
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string $surname
+     * @return User
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCPostal()
+    {
+        return $this->cPostal;
+    }
+
+    /**
+     * @param string $cPostal
+     * @return User
+     */
+    public function setCPostal($cPostal)
+    {
+        $this->cPostal = $cPostal;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImgProfile()
+    {
+        return $this->imgProfile;
+    }
+
+    /**
+     * @param string $imgProfile
+     * @return User
+     */
+    public function setImgProfile($imgProfile)
+    {
+        $this->imgProfile = $imgProfile;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRegisteredAt()
+    {
+        return $this->registered_at;
+    }
+
+    /**
+     * @param \DateTime $registered_at
+     * @return User
+     */
+    public function setRegisteredAt($registered_at)
+    {
+        $this->registered_at = $registered_at;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActivated()
+    {
+        return $this->activated;
+    }
+
+    /**
+     * @param bool $activated
+     * @return User
+     */
+    public function setActivated($activated)
+    {
+        $this->activated = $activated;
+        return $this;
+    }
+
+    
 }
